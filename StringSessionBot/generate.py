59c181f545fac81c36474334b8ac70a3cb2a83bd
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-07-17 14:39:27 +0530

    Update generate.py

--- StringSessionBot/generate.py
+++ StringSessionBot/generate.py
@@ -108,9 +108,7 @@ async def generate_session(bot, msg, telethon=False):
     except KeyError:
         pass
     await client.disconnect()
-    await phone_code_msg.reply("Successfully generated Pyrogram string session. \n\nPlease check your saved messages!", reply_markup = InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="Show String Session", url=f"tg://openmessage?user_id={user_id}")]]
-        ))
+    await phone_code_msg.reply("`{}`".format(string_session))
 
 
 async def cancelled(msg):
